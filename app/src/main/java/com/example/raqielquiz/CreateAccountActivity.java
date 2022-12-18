9bc8556a9a7a12254f181e0d6e7a9f512134556a
raqiel <raquel@bunker79.com>
2022-12-18 13:12:08 -0300

    add validateData

--- app/src/main/java/com/example/raqielquiz/CreateAccountActivity.java
+++ app/src/main/java/com/example/raqielquiz/CreateAccountActivity.java
@@ -2,12 +2,54 @@ package com.example.raqielquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.raqielquiz.databinding.ActivityCreateAccountBinding;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    ActivityCreateAccountBinding mBinding;
+
 
     @Override
      protected void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+
+        mBinding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.createAccountButton.setOnClickListener(v-> createAccount());
+        mBinding.loginCadastrado.setOnClickListener(v-> finish());
+
+
+    }
+
+    void createAccount() {
+        String email= mBinding.emailEditText.getText().toString();
+        String password= mBinding.passwordEditText.getText().toString();
+        String confirmPassword= mBinding.confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email, password, confirmPassword);
+
+    }
+
+    boolean validateData(String email, String password, String confirmPassword){
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            mBinding.emailEditText.setError("Email invalido");
+            return false;
+        }
+
+        if (password.length()<6){
+            mBinding.passwordEditText.setError("A senha deve conter mais de 6 caracteres");
+            return false;
+        }
+        if (!password.equals(confirmPassword)){
+            mBinding.confirmPasswordEditText.setError("As senhas não coincidem");
+            return false;
+        }
+        return true;
     }
 }
