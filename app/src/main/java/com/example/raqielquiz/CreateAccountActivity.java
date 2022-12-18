075e7e38bf3c73cf7aee5d5b72c74889a952704c
raqiel <raquel@bunker79.com>
2022-12-18 13:42:57 -0300

    create account add

--- app/src/main/java/com/example/raqielquiz/CreateAccountActivity.java
+++ app/src/main/java/com/example/raqielquiz/CreateAccountActivity.java
@@ -1,12 +1,18 @@
 package com.example.raqielquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.raqielquiz.databinding.ActivityCreateAccountBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -32,7 +38,45 @@ public class CreateAccountActivity extends AppCompatActivity {
         String confirmPassword= mBinding.confirmPasswordEditText.getText().toString();
 
         boolean isValidated = validateData(email, password, confirmPassword);
+        if(!isValidated){
+            return;
+        }
 
+        createAccountInFirebase(email, password);
+
+    }
+
+     void createAccountInFirebase(String email, String password) {
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccountActivity.this,
+        new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task){
+                if (task.isSuccessful()){
+                    Toast.makeText(CreateAccountActivity.this, "Conta criada com sucesso, confirme no seu email", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.getCurrentUser().sendEmailVerification();
+                    firebaseAuth.signOut();
+                    finish();
+                }else {
+
+                    Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
+        );
+    }
+
+    void changeInProgress(boolean inProgress){
+       if(inProgress){
+           mBinding.progressBar.setVisibility(View.VISIBLE);
+           mBinding.createAccountButton.setVisibility(View.GONE);
+       }else{
+           mBinding.progressBar.setVisibility(View.GONE);
+           mBinding.createAccountButton.setVisibility(View.VISIBLE);
+       }
     }
 
     boolean validateData(String email, String password, String confirmPassword){
